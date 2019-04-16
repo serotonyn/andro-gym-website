5150e9d2ac0c9f84c3030d60cab7ce6776d92330
serotonyn <youyou1youcef@gmail.com>
2019-04-16 13:06:50 +0100

    title bar & favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    name: `Khaber Youcef`,
-    title: `title`,
+    name: `Gym Software Management`,
+    title: `AndroGym`,
     date: `date`,
   },
   plugins: [
@@ -34,5 +34,36 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/slides/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
   ],
 };
